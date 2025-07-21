095cb749cd07ecf5248bf5a691a71ee077797aff
sanghyeom <tkdguss2001@naver.com>
2025-07-21 15:33:50 +0900

    1933_divisor_sol for if문 혼합 , 나머지 확인해서 나누어지는 소수 찾아서 거꾸로 프린트

--- swea/D1/swea_1933_N_divisor.py
+++ swea/D1/swea_1933_N_divisor.py
@@ -1,9 +1,9 @@
 N = int(input())
 listA = []
 
-print(N)
-print(type(N))
-
-for i in range(N+1) :
-    if type((N/i)) == type(N):
-        print(N/i)
+for i  in range(N+1) :
+    revers = N-i
+    if revers == 0 :
+        pass
+    elif N%revers == 0 :
+        print(int(N/revers),end=" ")
