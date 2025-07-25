2f739ea55b41d7239b7f5903895c8003073fc567
sanghyeom <tkdguss2001@naver.com>
2025-07-25 17:56:43 +0900

    greed

---
+++ swea/D2/re_swea_21425_+=.py
@@ -0,0 +1,24 @@
+T = int(input())
+
+for Test_case in range(1,T+1):
+    N = list(map(int,input().split()))
+    count = 1
+    if N[0] >=N[1]:
+        MAX = N[0]
+        MIN = N[1]
+    else :
+        MAX = N[1]
+        MIN = N[0]
+    MIN = MAX + MIN
+    while MAX < N[2] :
+        # print(N[2],MIN,MAX)
+        A = 0
+        MIN += MAX
+        if MAX <= MIN :
+            A = MAX
+            MAX = MIN
+            MIN = A
+        count +=1
+        if MAX > N[2] :
+            break
+    print(f'{count}')
