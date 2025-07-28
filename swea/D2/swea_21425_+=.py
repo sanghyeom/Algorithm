8c24c71560eed9258d994ee80c5626ab5e5b3e4f
sanghyeom <tkdguss2001@naver.com>
2025-07-28 17:10:19 +0900

    swea_21425 sol

--- swea/D2/swea_21425_+=.py
+++ swea/D2/swea_21425_+=.py
@@ -2,18 +2,18 @@ T = int(input())
 
 for Test_case in range(1,T+1):
     N = list(map(int,input().split()))
-    count = 1
+    count = 0
     if N[0] >=N[1]:
         MAX = N[0]
         MIN = N[1]
     else :
         MAX = N[1]
         MIN = N[0]
-    MIN = MAX + MIN
-    while MAX < N[2] :
+
+    while MAX <= N[2] :
         # print(N[2],MIN,MAX)
         A = 0
-        MIN += MAX
+        MIN = MAX + MIN
         if MAX <= MIN :
             A = MAX
             MAX = MIN
