b81e57cbdd276055989bb6f2f95c03f14a1a5597
sanghyeom <tkdguss2001@naver.com>
2025-08-04 14:18:43 +0900

    1206 sol

--- swea/D2/swea_1206_View.py
+++ swea/D2/swea_1206_View.py
@@ -1,11 +1,10 @@
-T = int(input())
-
-for Test_case in range(1,T+1):
+for Test_case in range(1,11):
     N = int(input())
+    ai = list(map(int,input().split()))
     total = 0
-    for i in range(1,N+1) :
-        if i %2 == 0 :
-            total -= i
-        else :
-            total += i
+    for i in range(2,N-2):
+        around_i = ai[i-2],ai[i-1],ai[i+2],ai[i+1]
+        if ai[i]>max(around_i) :
+            total += ai[i] - max(around_i)
+    
     print(f'#{Test_case} {total}')
