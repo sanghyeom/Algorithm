3a58e9b0812c56384c327209f68d296da0c06560
sanghyeom <tkdguss2001@naver.com>
2025-08-04 11:59:45 +0900

    4828 sol

--- swea/D2/swea_4828_minmax.py
+++ swea/D2/swea_4828_minmax.py
@@ -2,10 +2,14 @@ T = int(input())
 
 for Test_case in range(1,T+1):
     N = int(input())
+    ai = list(map(int,input().split()))
+    max_ai = 0
+    min_ai = ai[0]
     total = 0
-    for i in range(1,N+1) :
-        if i %2 == 0 :
-            total -= i
-        else :
-            total += i
+    for i in range(N) :
+        if ai[i] > max_ai :
+            max_ai = ai[i]
+        if ai[i] <= min_ai :
+            min_ai = ai[i]
+    total = max_ai - min_ai
     print(f'#{Test_case} {total}')
