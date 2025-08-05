dfef33ebd1d28d620c38860883fa97706e08b1d4
sanghyeom <tkdguss2001@naver.com>
2025-08-05 11:38:06 +0900

    푸는중

---
+++ swea/D2/ing_swea_12712_fry.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+for Test_case in range(1,T+1):
+    N , M= map(int,input().split())
+    pan = [0]* 
+    for i in range(N):
+        pan[i] = list(map(int,input().split()))
+    
+
+
+
+    print (pan)
+    print(f'#{Test_case} ')
--- swea/D2/swea_4828_minmax.py
+++ swea/D2/swea_4828_minmax.py
@@ -5,11 +5,5 @@ for Test_case in range(1,T+1):
     ai = list(map(int,input().split()))
     max_ai = 0
     min_ai = ai[0]
-    total = 0
-    for i in range(N) :
-        if ai[i] > max_ai :
-            max_ai = ai[i]
-        if ai[i] <= min_ai :
-            min_ai = ai[i]
-    total = max_ai - min_ai
+    total = max(ai) - min(ai)
     print(f'#{Test_case} {total}')
