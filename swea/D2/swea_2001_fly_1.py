78a1e871bab8ec0ef5e8ce3f3ce64e65517b55a8
sanghyeom <tkdguss2001@naver.com>
2025-08-06 14:01:59 +0900

    2001 : 부분순열

---
+++ swea/D2/swea_2001_fly_1.py
@@ -0,0 +1,20 @@
+T = int(input())
+for Test_case in range(1,T+1):
+    N , M= map(int,input().split())
+    # 파리 리스트 업로드
+    pan = []
+    for _ in range(N):
+        pan.append(list(map(int,input().split())))
+    
+    max_fly = 0 
+    for x in range(N-M+1):
+        for y in range(N-M+1):
+            fly_sum =0
+            for k in range(M):
+                for h in range(M):
+                    fly_sum += pan[x+k][y+h]
+            if fly_sum > max_fly:
+                max_fly = fly_sum
+
+
+    print(f'#{Test_case} {max_fly}')
