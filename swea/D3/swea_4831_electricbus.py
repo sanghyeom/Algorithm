41091396142370f70ca9c97deac8d728e89bf627
sanghyeom <tkdguss2001@naver.com>
2025-08-07 15:38:03 +0900

    __

--- swea/D3/swea_4831_electricbus.py
+++ swea/D3/swea_4831_electricbus.py
@@ -5,14 +5,18 @@ for Test_case in range(1,T+1):
     
     Charge_list = list(map(int,input().split())) # 충전기 위치
     Charge_list_convert = [0] * (N+1)
+
     for j in range(M) :
         Charge_list_convert[Charge_list[j]] += 1
     
     min_charge = 0 
     bus = 0
+    
     while bus + K <N :
         for findcharge in range(K-1,-1,-1):
+
             position = bus + findcharge + 1
+
             if Charge_list_convert[position] == 1 :                
                 min_charge +=1 
                 bus += findcharge+1
