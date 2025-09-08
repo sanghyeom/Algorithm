733db1827c9f10b64677cea8efbb57761f1057aa
sanghyeom <tkdguss2001@naver.com>
2025-09-08 15:53:17 +0900

    greedy 20551 compare

---
+++ swea/D3/20551.py
@@ -0,0 +1,27 @@
+T = int(input())
+for TC in range(1,T+1) : 
+    A,B,C = map(int,input().split())
+
+    result = -1
+    cnt = 0
+    
+    while True :
+        if C <= B : 
+            B -= 1
+            cnt +=1
+
+            if C == 0 or B == 0 or A == 0 :
+                break 
+
+        elif B <= A :
+            A -= 1
+            cnt += 1
+            if C == 0 or B == 0 or A == 0 :
+                break 
+        else : 
+            result = max(cnt,result)
+            break
+    
+    print(f'#{TC}'result)
+
+        
